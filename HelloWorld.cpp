79f672e45a8445e12a08d9ed234e955683719b4b
Jordi-Ab <jordi_a3@hotmail.com>
2016-10-10 16:57:21 +0100

    Made some slight changes using QtCreator

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,7 +1,12 @@
-#include <iostrem>
+#include <iostream>
 using namespace std;
 
+/*
+ * This file is compiled and modified using Qt Creator, but the cpp file is on github.
+ */
+
+//Prints Hello World in the console screen.
 int main(){
-	cout << "Hello World, I'm in C++";
+    cout << "Hello World, I'm in C++" << endl;
 	return 0;
 }
